17cade42c7609497c8ec71e21d27c36c16cd6aa9
Germen Wong <1953792796@qq.com>
2024-07-03 10:59:21 +0800

    完善Result、Error

--- app/src/main/java/com/hgm/cleanarcherrorhandling/domain/Error.kt
+++ app/src/main/java/com/hgm/cleanarcherrorhandling/domain/Error.kt
@@ -5,3 +5,6 @@ package com.hgm.cleanarcherrorhandling.domain
 // 通过实现 Error 这个接口我们可以自定义很多错误信息组
 // 验证错误、网络错误、配置错误等等
 sealed interface Error
+
+// 使用别名防止与Kotlin Error类重名
+typealias RootError = Error
--- app/src/main/java/com/hgm/cleanarcherrorhandling/domain/Result.kt
+++ app/src/main/java/com/hgm/cleanarcherrorhandling/domain/Result.kt
@@ -1,11 +1,22 @@
 package com.hgm.cleanarcherrorhandling.domain
 
 
-// 使用别名防止与Kotlin Error类重名
-typealias RootError = Error
 
 // 接收任何类型的数据和错误信息
 sealed interface Result<out D, out E : RootError> {
-      data class Success<out D, out E : RootError>(val data: D) : Result<D, E>
-      data class Error<out D, out E : RootError>(val message:E) : Result<D, E>
+      data class Success<out D>(val data: D) : Result<D, Nothing>
+      data class Error<out E : RootError>(val message: E) : Result<Nothing, E>
 }
+
+inline fun <T, E : RootError, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+      return when (this) {
+            is Result.Error -> Result.Error(message)
+            is Result.Success -> Result.Success(map(data))
+      }
+}
+
+fun <T, E : RootError> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
+      return map { }
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
